816e2febc9f7114a7be3e06ab3abbe50a2e1fadc
igordev-der <igor.d86@gmail.com>
2020-12-21 21:03:09 +0100

    Photo icon added

--- example/lib/app/widgets/telegram_post/post/content_widgets/messagePicture_post/messagePhoto_post.controller.dart
+++ example/lib/app/widgets/telegram_post/post/content_widgets/messagePicture_post/messagePhoto_post.controller.dart
@@ -7,6 +7,10 @@ import 'package:telegram_service/td_api.dart' as tdapi;
 import 'dart:convert';
 
 class MessagePhotoPostContentController extends MvcController {
+  static const int IMAGE_READY_STATE = MvcController.NORMAL_STATE;
+  static const int IMAGE_LOADING_STATE = MvcController.LOADING_STATE;
+  static const int IMAGE_NOT_LOADED_STATE = -MvcController.LOADING_STATE;
+
   final TelegramChannelMessageInfo messsageInfo;
 
   Uint8List thumbnailBytes;
@@ -15,7 +19,8 @@ class MessagePhotoPostContentController extends MvcController {
       (messsageInfo.content as tdapi.MessagePhoto);
 
   MessagePhotoPostContentController(this.messsageInfo)
-      : assert(messsageInfo.content is tdapi.MessagePhoto);
+      : assert(messsageInfo.content is tdapi.MessagePhoto),
+        super(startingState: IMAGE_NOT_LOADED_STATE);
 
   String get postText => messageContent.caption.text;
 
--- example/lib/app/widgets/telegram_post/post/content_widgets/messagePicture_post/messagePhoto_post.view.dart
+++ example/lib/app/widgets/telegram_post/post/content_widgets/messagePicture_post/messagePhoto_post.view.dart
@@ -2,10 +2,11 @@ import 'dart:ui';
 
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
-import 'package:telegram_service/td_api.dart';
+import 'package:telegram_service/td_api.dart' as tdapi;
 import 'package:telegram_service_example/app/model/message_info.dart';
 import 'package:telegram_service_example/app/widgets/telegram_post/post/content_widgets/collapsable_content/collapsable_content.view.dart';
 import 'package:telegram_service_example/utils/telegram/posts_builders/post_content_widget.dart';
+import 'package:id_mvc_app_framework/framework.dart';
 
 import 'messagePhoto_post.controller.dart';
 
@@ -22,52 +23,92 @@ class MessagePhotoPostContent
   @override
   Widget buildMain() => CollapsablePostContent(
         postText: c.postText,
-        mediaContent: _postMediaContent1,
+        mediaContent: GestureDetector(
+          onTap: () {
+            // TODO : establish on tap media content onTap handling
+            Get.log('${c.messsageInfo.id}');
+          },
+          child: Stack(
+            children: [
+              _mediaContent,
+              _mediaContentStateIcon,
+            ],
+          ),
+        ),
+      );
+
+  Widget get _mediaContent => AspectRatio(
+        aspectRatio: c.aspectRatio,
+        child: FittedBox(
+          fit: BoxFit.fitWidth,
+          child: _mediaContentByState,
+        ),
+      );
+
+  Widget get _mediaContentStateIcon => AspectRatio(
+        aspectRatio: c.aspectRatio,
+        child: Align(
+          alignment: Alignment.center,
+          child: Container(
+            child: Icon(
+              Icons.cloud_download_outlined,
+              size: 40,
+              color: Colors.grey[850],
+            ).paddingAll(5),
+            decoration: BoxDecoration(
+              color: Colors.grey[850].withOpacity(0.2),
+              borderRadius: BorderRadius.circular(40),
+            ),
+          ),
+        ),
       );
 
   // TODO : Implement Post media content handling
-  Widget get _postMediaContent => AspectRatio(
+  Widget get _mediaPlaceholder => AspectRatio(
         aspectRatio: c.aspectRatio,
         child: Placeholder(
           color: Colors.yellow,
         ),
       );
 
-  Widget get _postMediaContent1 => AspectRatio(
-        aspectRatio: c.aspectRatio,
-        child: FittedBox(
-          child: Stack(
-            children: [
-              Container(
-                width: c.picWidth,
-                height: c.picHeight,
-                decoration: BoxDecoration(
-                  image: DecorationImage(
-                    image: c.minithumbnail,
-                    fit: BoxFit.fitWidth,
-                  ),
-                ),
+  Widget get _mediaContentByState {
+    switch (c.state) {
+      case MessagePhotoPostContentController.IMAGE_NOT_LOADED_STATE:
+        return _getThumbnail;
+      default:
+        return _mediaPlaceholder;
+    }
+  }
+
+  Widget get _getThumbnail => Stack(
+        children: [
+          Container(
+            width: c.picWidth,
+            height: c.picHeight,
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: c.minithumbnail,
+                fit: BoxFit.fitWidth,
               ),
-              Container(
-                width: c.picWidth,
-                height: c.picHeight,
-                child: ClipRect(
-                  child: BackdropFilter(
-                    filter: ImageFilter.blur(sigmaX: 2, sigmaY: 2),
-                    child: Container(
-                      color: Colors.black.withOpacity(0.2),
-                    ),
-                  ),
-                ),
-              ),
-            ],
+            ),
           ),
-          fit: BoxFit.fitWidth,
-        ),
+          Container(
+            width: c.picWidth,
+            height: c.picHeight,
+            child: ClipRect(
+              child: BackdropFilter(
+                filter: ImageFilter.blur(sigmaX: 2, sigmaY: 2),
+                child: Container(
+                  color: Colors.black.withOpacity(0.2),
+                ),
+              ),
+            ),
+          ),
+        ],
       );
 
   @override
-  List<String> get contentTypes => [MessagePhoto.CONSTRUCTOR];
+  List<String> get contentTypes => [tdapi.MessagePhoto.CONSTRUCTOR];
 
   static PostContentWidget builder(TelegramChannelMessageInfo messageInfo) =>
       MessagePhotoPostContent(
