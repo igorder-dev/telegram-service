25578660766da68ad5336273f668922694a63abc
Pavel Fedorov <fedorovpa@gmail.com>
2020-12-24 19:27:26 -0800

    Added code documentation and read/write in handler

--- example/lib/app/model/hive_storage_service.dart
+++ example/lib/app/model/hive_storage_service.dart
@@ -8,38 +8,40 @@ class HiveStorageService<T> extends StorageServiceBase<T> {
   T _data;
   Iterable<String> _allKeys;
 
+  // constructor with default box name
   HiveStorageService(String boxName, {String defaultKey = 'default'})
       : assert(boxName != null),
         _boxName = boxName,
         _defaultKey = 'default',
         super();
 
+  // Closing the hive box
   @override
   void dispose() async {
     assert(isLoaded, 'Box $_boxName has to be loaded first');
     await _box.close();
   }
-
+  // Checking the open status
   @override
   bool get isLoaded => _box != null;
 
   // To match StorageServiceBase
   @override
   Future<T> load() async {
-    await loadWithKey(_defaultKey);
+    return await loadWithKey(_defaultKey);
   }
 
   @override
   Future<void> save() async {
     await saveWithKey(_defaultKey);
   }
-
+  // Main save/load wrappers
   Future<void> saveWithKey(String key) async {
     assert(isLoaded, 'Box $_boxName has to be loaded first');
     await _box.put(key, _data);
   }
 
-  Future<void> loadWithKey(String key) async {
+  Future<T> loadWithKey(String key) async {
     _box = await getOpenBox();
     return _box.get(key);
   }
@@ -51,6 +53,7 @@ class HiveStorageService<T> extends StorageServiceBase<T> {
   @override
   set data(T value) => _data = value;
 
+  // List of keys for iterating through them
   List<dynamic> get allKeys => (isLoaded ? _box.keys.toList() : 0);
 
   // To prevent opening new box every time
@@ -60,4 +63,7 @@ class HiveStorageService<T> extends StorageServiceBase<T> {
     }
     return Hive.box(_boxName);
   }
+
+  // Checking if item is present in the box
+  bool contains(String key) => (isLoaded ? _box.containsKey(key) : false);
 }
--- example/lib/utils/telegram/handlers/telegram_chats_handler.dart
+++ example/lib/utils/telegram/handlers/telegram_chats_handler.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:id_mvc_app_framework/framework.dart';
 import 'package:id_mvc_app_framework/model.dart';
 import 'package:telegram_service/td_api.dart';
@@ -7,6 +9,7 @@ import 'package:telegram_service_example/app/model/channel_info_store.dart';
 import 'package:telegram_service_example/app/model/message_info.dart';
 import 'package:telegram_service_example/app/model/message_info_store.dart';
 import 'package:telegram_service_example/utils/telegram/posts_builders/post_content_builder_service.dart';
+import 'package:telegram_service_example/app/model/hive_storage_service.dart';
 
 class TdlibChatsHandler extends TelegramEventHandler with GetxServiceMixin {
   static TdlibChatsHandler get instance => Get.find();
@@ -35,18 +38,42 @@ class TdlibChatsHandler extends TelegramEventHandler with GetxServiceMixin {
     }
   }
 
-  void _handleMessagesEvent(Messages messages) {
+  void _handleMessagesEvent(Messages messages) async{
     final messagesStore = TelegramChannelMessageInfoStore();
-    messages.messages.forEach((message) {
+    messages.messages.forEach((message) async {
       final messageInfo = TelegramChannelMessageInfo.fromMessage(message);
       if (TelegramPostContentBuilderService.hasBuilder(messageInfo)) {
-        messagesStore[message.id] =
-            TelegramChannelMessageInfo.fromMessage(message);
+
+        // Saving in Hive / restoring from Hive
+        String messageKey = message.id.toString();
+
+        // Getting the messages box and initializing it
+        var storage = HiveStorageService<String>('messages');
+        await storage.load();
+
+        // Checking if message is already stored in the chats box
+        if (storage.contains(messageKey)){
+          // Adding to messagesStore from chats box
+          messagesStore[message.id] = TelegramChannelMessageInfo.fromJson(jsonDecode(await storage.loadWithKey(messageKey)));
+        }else{
+          messagesStore[message.id] =
+              TelegramChannelMessageInfo.fromMessage(message);
+          storage.data= jsonEncode(TelegramChannelMessageInfo.fromMessage(message));
+          await storage.saveWithKey(messageKey);
+
+          Get.log("_handleMessagesEvent: Message ID${message.id} saved to Hive successfully");
+        }
+
+        // Closing the messages box
+        storage.dispose();
+
         Get.log(
             "_handleMessagesEvent: [${message.chatId}] - [${message.id}] - [${message.content.getConstructor()} - added]");
 
         // serialization test
-        Get.log("JSON serialized message: ${TelegramChannelMessageInfo.fromMessage(message).toJson()}");
+        //Get.log("JSON serialized message: ${TelegramChannelMessageInfo.fromMessage(message).toJson()}");
+
+
 
       } else {
         Get.log(
@@ -55,14 +82,41 @@ class TdlibChatsHandler extends TelegramEventHandler with GetxServiceMixin {
     });
   }
 
-  void _handleChatEvent(Chat chat) {
+  void _handleChatEvent(Chat chat) async{
     if (chat.type.getConstructor() != ChatTypeSupergroup.CONSTRUCTOR) return;
     final channelsStore = TelegramChannelInfoStore();
     getChatMessages(chat.id);
-    channelsStore[chat.id] = TelegramChannelInfo.fromChat(chat);
 
     // serialization test
-    Get.log("JSON serialized channel: ${TelegramChannelInfo.fromChat(chat).toJson()}");
+    // Get.log("JSON serialized channel: ${TelegramChannelInfo.fromChat(chat).toJson()}");
+
+    // Saving in Hive / restoring from Hive
+    String chatKey = chat.id.toString();
+
+    // Getting the chats box and initializing it
+    var storage = HiveStorageService<String>('chats');
+    await storage.load();
+
+    // Checking if chat is already stored in the chats box
+    if (storage.contains(chatKey)){
+
+      // Adding to channelsStore from chats box
+      channelsStore[chat.id] = TelegramChannelInfo.fromJson(jsonDecode(await storage.loadWithKey(chatKey)));
+      Get.log(
+          "_handleMessagesEvent: Chat ID${chat.id} loaded from Hive successfully");
+    }else {
+      // Adding to channelsStore from chat
+      channelsStore[chat.id] = TelegramChannelInfo.fromChat(chat);
+
+      // Saving to chats box
+      storage.data = jsonEncode(channelsStore[chat.id]);
+      await storage.saveWithKey(chat.id.toString());
+      Get.log(
+          "_handleMessagesEvent: Chat ID${chat.id} saved to Hive successfully");
+    }
+
+    // Closing the chats box
+    storage.dispose();
   }
 
   void _handleChatsEvent(Chats chats) {
